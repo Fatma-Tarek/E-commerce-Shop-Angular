861fb7c355223d13ae4f8432a7c268100034da8f
[suzan naeem] <[suzannaim7@gmail.com]>
2021-04-19 05:36:41 +0200

    add shopping cart

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product.model';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -7,9 +8,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
+  selectedproducts: any[];
+  // status: boolean;
+  // total: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCart();
+    // this.selectedproducts = JSON.parse(localStorage.getItem('localCart'));
+    // this.total = 0;
+    // for(let selectedproduct of this.selectedproducts){
+    //   this.total = this.total + (selectedproduct.Price * selectedproduct.Quantity);
+    // }
+    // // this.status = false;
+    // console.log(this.selectedproducts);
   }
 
-}
+  Delete(index) {
+    this.selectedproducts.splice(index,1);
+    localStorage.setItem('localCart',JSON.stringify(this.selectedproducts));
+  }
+
+  Save(index) {
+    localStorage.setItem('localCart',JSON.stringify( this.selectedproducts));
+  }
+
+  Checkout(index) {
+    // this.status = true;
+  }
+
+  Delete_All() {
+    this.selectedproducts = [];
+    localStorage.clear();
+    this.total = 0;
+
+  }
+
+    incQnt(ProductId,num, Quantity ){
+    for(let i=0; 
+    i<this.selectedproducts.length;
+    i++){
+    if
+    (this.selectedproducts[i].ProductId 
+      === ProductId){
+    if(num < this.selectedproducts[i].Quantity)
+    this.selectedproducts[i].num
+     = parseInt(num) + 1;
+    }
+    }
+
+    
+    
+    localStorage.setItem
+    ('localCart', 
+    JSON.stringify
+    (this.selectedproducts));
+     this.loadCart();
+    }
+    
+    decQnt(ProductId,num ){
+    for
+    (let i=0; 
+    i<this.selectedproducts.length;
+     i++){
+    if
+    (this.selectedproducts[i].ProductId
+      === ProductId){
+    if(num!= 1)
+    this.selectedproducts[i].num
+    = parseInt(num) - 1;
+    }
+    }
+    localStorage.setItem
+    ('localCart', 
+    JSON.stringify
+    (this.selectedproducts));
+     this.loadCart();
+    }
+
+    total:number = 0;
+    loadCart(){
+    if(localStorage.getItem('localCart')){
+    this.selectedproducts = JSON.parse(localStorage.getItem('localCart'));
+    this.total = this.selectedproducts.reduce(function(acc, val){
+return acc + (val.Price * val.Quantity);
+    }, 0);
+     }
+     }
+     }
+
